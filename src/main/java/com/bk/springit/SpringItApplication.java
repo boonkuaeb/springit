5db9770d26aaf00622ccb5aea80f1949232690b3
BOONKUAE BOONSUTTA <boonkuae.boonsutta@scb.co.th>
2020-04-21 22:51:48 +0700

    Look into properties of Spring Boot.

--- src/main/java/com/bk/springit/SpringItApplication.java
+++ src/main/java/com/bk/springit/SpringItApplication.java
@@ -1,13 +1,28 @@
 package com.bk.springit;
 
+import com.bk.springit.config.SpringProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringProperties.class)
 public class SpringItApplication {
+
+	@Autowired
+	private SpringProperties springProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringItApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner()
+	{
+		return args -> System.out.println(springProperties.getWelcomMsg());
 	}
 
 }
