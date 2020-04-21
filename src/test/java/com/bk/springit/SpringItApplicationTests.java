21778006c36b33f1ac1f60708d7e09380afce190
BOONKUAE BOONSUTTA <boonkuae.boonsutta@scb.co.th>
2020-04-21 22:10:51 +0700

    Init project

---
+++ src/test/java/com/bk/springit/SpringItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bk.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
